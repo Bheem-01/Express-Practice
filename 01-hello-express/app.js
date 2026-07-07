c7ee9e832d991ddeacc7af8d384dfaa477c0c15d
Bheem <bheemsharma0111@gmail.com>
2026-07-07 09:54:42 +0530

    Created my first server

--- 01-hello-express/app.js
+++ 01-hello-express/app.js
@@ -1,11 +1,14 @@
+// Import the Express framework.
 const express = require("express");
+
+// Create an Express application
 const app = express();
 
-let port = 3000;
+// Start the server.
+const port = 3000;
 app.listen(port, ()=>{
     console.log("Server started successfully");
-    console.log(`Listening on port ${port}`);
-});
+    console.log(`Server running at http://localhost:${port}`);});
 
 /*// Import the Express framework.
 // Express helps us create web servers and handle HTTP requests easily.
